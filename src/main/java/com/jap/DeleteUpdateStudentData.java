00ec23c6a864f92261c7b3572fd555f00a957eea
rohitsnarnaware7@gmail.com <rohitsnarnaware7@gmail.com>
2022-09-12 22:46:26 +0530

    completed with developed connection and executed queries

--- src/main/java/com/jap/DeleteUpdateStudentData.java
+++ src/main/java/com/jap/DeleteUpdateStudentData.java
@@ -10,7 +10,16 @@ public class DeleteUpdateStudentData
 {
 
     public Connection getConnection(){
-        Connection connection=null;
+        String url = "jdbc:mysql://localhost:3306/school";
+        String user = "root";
+        String pass = "admin";
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         return connection;
 
@@ -18,12 +27,32 @@ public class DeleteUpdateStudentData
 
     public void deleteStudentData(){
       // call getConnection() method
-       
+        try {
+            String query = "Delete from student.student where name = 'Karade'";
+            Statement statement = getConnection().createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Deleted ");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         //execute delete query
     }
 
     public void updateStudentData(){
         // call getConnection() method
+        Statement statement = null;
+        try {
+            statement = getConnection().createStatement();
+            String query = "UPDATE  student.student SET name = 'Kaka' where name = 'Rohit'";
+            statement.executeUpdate(query);
+            System.out.println("Database values updated");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
         //execute update query
     }
